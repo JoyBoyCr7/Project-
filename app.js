ca4949864120f3fbae502f3f55dbc45056f36857
Ronard Nyongkah <ronardnyongkah@Ronards-MacBook-Air.local>
2023-05-26 19:48:03 -0500

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 
 console.log("ronard")
-console.log($)
+
+let response = $.ajax("https://www.balldontlie.io/api/v1/players/?search=lebron")
+.then((response)=>{
+    console.log(response)
+    console.log(response.data)
+    let firstName = response.data[0].first_name
+    let lastName = response.data[0].last_name
+    console.log(`The Goat, ${firstName} ${lastName} has been summined`)
+    
+    // let players = response.data.map((player)=>{
+    //     console.log(player)
+    // })
+   
+    }
+    )
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Project 1
+Ronard Nyongkah
+
+### Project Description
+
+This project gets you facts about your favorite player. Like their height, weight, team ect. All that needs to be done is for you to enter the players name.
+
+## API Description
+
+- The API I'm using is https://www.balldontlie.io/home.html#getting-started
+
+- This is an example of me testing the API: $.ajax("https://www.balldontlie.io/api/v1/players/?search=lebron")
+
